d3cf1020e51f7ceb396ea4b8757053807824f82f
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-23 22:53:18 +0430

    Add files via upload

---
+++ path.go
@@ -0,0 +1,30 @@
+import os:
+import File:
+import System.Manger;
+import Lib.so*;
+import Path;
+print "Test"
+ if {
+ GetSystem Path Mix}
+ filed Masseges();
+ (
+ dex.file(android);
+ Sopath.Filemod.Get=setlibpath();
+ }else{
+ Get.System.LoadLibrary(ppssppjni);
+ }
+  });
+  if Get.scelet(go);
+  i++();
+  listArry<Storm File>,
+  Contcesh.Get.Listview1();
+  String path+3;
+  Input_Raw().org.ppsspp.ppsspp();
+   }
+      }
+           }
+               }
+               { 
+                 {
+                 {};
+        
