461aced6db292dbf73869d2dd35a2fdbf9f1168f
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-24 10:39:00 +0430

    Shell+cpp

---
+++ jni/cdm.sh
@@ -0,0 +1,3 @@
+xcopy ..\assets\flash0 assets\flash0\ /s /y <d.txt xcopy ..\assets\lang assets\lang\ /s /y <d.txt xcopy ..\assets\shaders assets\shaders\ /s /y <d.txt copy ..\assets\langregion.ini assets\langregion.ini copy ..\assets\compat.ini assets\compat.ini copy ..\assets\Roboto-Condensed.ttf assets\Roboto-Condensed.ttf copy ..\assets\*.png assets\ SET NDK=C:\Android\sdk\ndk\21.3.6528147 REM SET NDK=C:\Android\ndk SET NDK_MODULE_PATH=..\ext;..\ext\native\ext %NDK%/ndk-build -j32 
+xcopy ..res\fike.text\sdcard/psp/ninjacoder
+%*
