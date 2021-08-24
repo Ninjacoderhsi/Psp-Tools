24cf1a43a1672c91c54cc2c4ccae2fdd570f4c1a
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-24 10:28:58 +0430

    Add files via upload

---
+++ jni/main.cpp
@@ -0,0 +1,47 @@
+#include <test.h>
+#include <string>
+#include <main.cpp>
+#include <main.c>
+#include <pspsdk.cpp>
+
+
+using namespace srt;
+using namespace srt2;
+using namespace verical;
+using namespace horsintal;
+using namespace textview;
+using namespace edittext;
+using namespace listview;
+using namespace drawel;
+using namespace main;
+
+int main()
+{
+    if new==srt1;
+    filedir:("c/psp/");
+    filedir:("c/psp%Game%savedata%")
+    filedir:("c/psp%textures")
+    }else{
+           show.massges("filed,to,make,folder/n");
+     };   
+        
+}
+{
+    string new == new.folder.setTimeout;
+    new timer = timerask("seemaketofolder")
+    file== timerask(3000);
+    if{
+        show.massges("ok");
+        
+    }
+    
+    listview1== set <arrylistmap>
+    listmap;
+    listmap== onChange("set Pathdir");
+    listmap== onbindcustomview("/sdcard/");
+    onbindcustomview(get.textview1("roda.filepath")))));
+    onbindcustomview(Glide.with(getApplicationContext()).load(Uri.parse(_data.get((int)_position).get.toString())).into(img_content);
+    
+   android.on();
+   set.pathfile();
+}
