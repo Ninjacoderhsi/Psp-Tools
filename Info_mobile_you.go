ff5e8d47d3b5aed464ed1e622afb8eb9fa1f0379
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-26 17:06:28 +0430

    Add files via upload

---
+++ Info_mobile_you.go
@@ -0,0 +1,237 @@
+
+paritytype Final Exsport interrupts()
+
+   import(
+   "Os"
+     "Fileutlie"
+     "mtv"
+     "funder"
+     "max"
+     "androidlibraryv1.0.0"
+     "maxline"
+     "batry"
+     "amper"
+     "colorhex"
+     "librarycontext"
+     "libraryifandelse"
+     "moblieinfo"
+     )
+     <libraryjavademo>
+     ////////info Mobile You//////
+     ///row Patht You Costume////
+     
+  private ScrollView vscroll1;
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private LinearLayout linear3;
+	private LinearLayout linear6;
+	private LinearLayout linear8;
+	private TextView textview1;
+	private LinearLayout linear4;
+	private LinearLayout linear5;
+	private TextView textview2;
+	private TextView jam;
+	private TextView tanggal;
+	private TextView textview5;
+	private TextView textview6;
+	private TextView brand;
+	private TextView textview8;
+	private LinearLayout linearbattery;
+	private LinearLayout linear19;
+	private LinearLayout line1;
+	private LinearLayout line2;
+	private LinearLayout line3;
+	private LinearLayout line4;
+	private LinearLayout line5;
+	private LinearLayout line6;
+	private TextView persen1;
+	private TextView textview32;
+	private TextView persen2;
+	private TextView peringatan;
+	private TextView textview10;
+	private LinearLayout linear9;
+	private LinearLayout linear10;
+	private LinearLayout linear11;
+	private LinearLayout linear12;
+	private LinearLayout linear13;
+	private LinearLayout linear14;
+	private LinearLayout linear15;
+	private LinearLayout linear16;
+	private LinearLayout linear17;
+	private LinearLayout linear18;
+	private LinearLayout linear20;
+	private TextView textview11;
+	private TextView textview12;
+	private TextView serial;
+	private TextView textview14;
+	private TextView textview15;
+	private TextView product;
+	private TextView textview16;
+	private TextView textview17;
+	private TextView id;
+	private TextView textview18;
+	private TextView textview19;
+	private TextView version;
+	private TextView type;
+	private TextView textview21;
+	private TextView user;
+	private TextView textview22;
+	private TextView textview23;
+	private TextView cpu;
+	private TextView textview24;
+	private TextView textview25;
+	private TextView host;
+	private TextView textview26;
+	private TextView textview27;
+	private TextView hardware;
+	private TextView textview28;
+	private TextView textview29;
+	private TextView fingerprint;
+	private TextView textview30;
+	private TextView textview31;
+	private TextView display;
+	private TextView textview33;
+	private TextView textview34;
+	private TextView realease;
+     
+     public class MainActivity extends AppCompatActivity
+     
+   textview1.getType("60/%")  
+   textview2.getType("Timer")
+   jam.select("Timer")
+   serial.get.phone(aderrs)
+   product.get.phone("product")
+   id.get.phone("idphone")
+   version.get.phone("sdkvresion")
+   user.get.phone("usernamberid")
+   cpu.get.phone.("iphone select Get.cpu info")
+   host.get.phone("ip")
+   hardward.get.phone("get.view.hardware")
+   fingerprint.get.phone("task")
+   display.get.phone.print("task")
+   realease.get.phone.fuuloor("task")
+   
+   (
+   
+   linear1.get.colorbackground(#203211a),sergardiliner(20)
+   
+   linear2.get.colorbackground(#203211a),sergardiliner(20)
+   
+   linear3.get.colorbackground(#203211a),sergardiliner(20)
+   
+   linear4.get.colorbackground(#203211a),sergardiliner(20)
+   
+   linear5.get.colorbackground(#203211a),sergardiliner(20)
+   linear6.get.colorbackground(#203211a),sergardiliner(20)
+   
+   
+   
+   )
+   
+   
+   (
+   
+   private void initialize(Bundle _savedInstanceState) {
+		
+		vscroll1 = findViewById(R.id.vscroll1);
+		linear1 = findViewById(R.id.linear1);
+		linear2 = findViewById(R.id.linear2);
+		linear3 = findViewById(R.id.linear3);
+		linear6 = findViewById(R.id.linear6);
+		linear8 = findViewById(R.id.linear8);
+		textview1 = findViewById(R.id.textview1);
+		linear4 = findViewById(R.id.linear4);
+		linear5 = findViewById(R.id.linear5);
+		textview2 = findViewById(R.id.textview2);
+		jam = findViewById(R.id.jam);
+		tanggal = findViewById(R.id.tanggal);
+		textview5 = findViewById(R.id.textview5);
+		textview6 = findViewById(R.id.textview6);
+		brand = findViewById(R.id.brand);
+		textview8 = findViewById(R.id.textview8);
+		linearbattery = findViewById(R.id.linearbattery);
+		linear19 = findViewById(R.id.linear19);
+		line1 = findViewById(R.id.line1);
+		line2 = findViewById(R.id.line2);
+		line3 = findViewById(R.id.line3);
+		line4 = findViewById(R.id.line4);
+		line5 = findViewById(R.id.line5);
+		line6 = findViewById(R.id.line6);
+		persen1 = findViewById(R.id.persen1);
+		textview32 = findViewById(R.id.textview32);
+		persen2 = findViewById(R.id.persen2);
+		peringatan = findViewById(R.id.peringatan);
+		textview10 = findViewById(R.id.textview10);
+		linear9 = findViewById(R.id.linear9);
+		linear10 = findViewById(R.id.linear10);
+		linear11 = findViewById(R.id.linear11);
+		linear12 = findViewById(R.id.linear12);
+		linear13 = findViewById(R.id.linear13);
+		linear14 = findViewById(R.id.linear14);
+		linear15 = findViewById(R.id.linear15);
+		linear16 = findViewById(R.id.linear16);
+		linear17 = findViewById(R.id.linear17);
+		linear18 = findViewById(R.id.linear18);
+		linear20 = findViewById(R.id.linear20);
+		textview11 = findViewById(R.id.textview11);
+		textview12 = findViewById(R.id.textview12);
+		serial = findViewById(R.id.serial);
+		textview14 = findViewById(R.id.textview14);
+		textview15 = findViewById(R.id.textview15);
+		product = findViewById(R.id.product);
+		textview16 = findViewById(R.id.textview16);
+		textview17 = findViewById(R.id.textview17);
+		id = findViewById(R.id.id);
+		textview18 = findViewById(R.id.textview18);
+		textview19 = findViewById(R.id.textview19);
+		version = findViewById(R.id.version);
+		type = findViewById(R.id.type);
+		textview21 = findViewById(R.id.textview21);
+		user = findViewById(R.id.user);
+		textview22 = findViewById(R.id.textview22);
+		textview23 = findViewById(R.id.textview23);
+		cpu = findViewById(R.id.cpu);
+		textview24 = findViewById(R.id.textview24);
+		textview25 = findViewById(R.id.textview25);
+		host = findViewById(R.id.host);
+		textview26 = findViewById(R.id.textview26);
+		textview27 = findViewById(R.id.textview27);
+		hardware = findViewById(R.id.hardware);
+		textview28 = findViewById(R.id.textview28);
+		textview29 = findViewById(R.id.textview29);
+		fingerprint = findViewById(R.id.fingerprint);
+		textview30 = findViewById(R.id.textview30);
+		textview31 = findViewById(R.id.textview31);
+		display = findViewById(R.id.display);
+		textview33 = findViewById(R.id.textview33);
+		textview34 = findViewById(R.id.textview34);
+		realease = findViewById(R.id.realease);
+   @Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	)
+   
+   
+   
+   )
