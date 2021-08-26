13dcdfbf927e0434c9467f7857785d5e632d9da8
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-26 12:20:56 +0430

    Add files via upload

---
+++ path2.go
@@ -0,0 +1,58 @@
+Psp Path Surece Data
+$ pull
+$ Get Path prx Files find((()())));;;
+$ Get map PRX Storam;
+ if {
+     Get.cpuSystem Data Path Suruce Data
+     import
+     "Os"
+     "system"
+     "path Oa"
+     "path Psp Sdk"
+     "splashing"
+     "hash"
+     "png"
+     "jpg"
+     "veictor"
+     "svg"
+     ;;;;;;;;;;;;;;
+    
+ }
+ {
+     Png = Png.getBytes();
+     jpg = jpg.getBytes();
+     ;;;;;;;;
+ }
+ 
+ {
+     veictor = veictor.getType(.xml);
+     svg = svg.os.pathgettypeview(web.constrain);
+     if i++();
+     webview1.google.path.system.res();
+     webview1.ture.html("yes");
+     webview1.fasle.javas("not");
+     }
+      }
+        }
+           }
+           {
+               
+           }
+   ///////  main.cpp
+   
+   get.path"main.cpp"
+   get.path"libhelp.cpp"
+   find.dex1();
+   get.path("c/cd/psp");
+   
+   if%1s == Cpu.get.mail();
+   show.msg("psp Tools V 1.1.1 \n Not finds Path");
+   
+   
+   
+   import
+   "makefile"
+   <tools>
+   ;;
+ }
+ 
