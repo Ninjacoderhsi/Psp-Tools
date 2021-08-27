553309c63f94f3fbf28ad8354e2a6e9a59eee849
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-27 19:36:03 +0430

    Add files via upload

---
+++ Codec#/iniclass.cs
@@ -0,0 +1,84 @@
+////
+///psp Tools New Soft Ware Best 
+//*
+
+///////PSP TOOS NEW SOFT UPDATE IN PPSSPP
+
+using System;
+using Edittext;
+using Os;
+using TextView;
+using Log;
+using context;
+using Path/main.cpp;
+using String;
+using Bool;
+
+
+public class ini Editor
+{
+    static public void Main ()
+    {
+    private textview1;
+    private imageview1;
+    private text1;
+    private text2;
+    private text3;
+    private textview14;
+    private imageview2;
+    private text4;
+    private text6;
+    private text5;
+    private textview33;
+    private imageview3;
+    private text9;
+    private text10;
+    private text11;
+    
+    {
+        text1 == ("text1.r.layout.id.text1");
+        
+       text2 == ("text2.r.layout.id.text2");
+       
+       text4 == ("text4.r.layout.id.text4");
+       
+            text3 == ("text4.r.layout.id.text4");  
+        
+//////get Edit Add Ids Tankes By Ninja Coder.ir
+      
+    }
+   clipborad.manager.new==("get_import_textfor_TextView_Get_PastData_For_EDITTEXT[]
+   String Hsimod == Hsimod.setcontext();
+  /// Hsimod.get.racsuse(******);
+  Hsimod == GetpastData("clipborad_manager");
+  if 12=0())));
+  importdata =("hsimod");
+  get,FileShard*);
+  }
+      }
+  {
+       
+       public iniEditor
+       new.context("inieditor");
+       get.button1("$");
+       button1==@oneclick{
+           clipboard.manager("gettext.hsimod");
+           Cilpbord.PastText(hsimod);
+           tosga.app("انجام شد");
+           
+       }
+       text1==@oneclick{
+           androidonClick.("/}");
+       }
+       {
+           text2==@oneclick{
+               androidonClick("/}");
+           }
+           text3==@oneclick{
+               androidonClick("/.");
+              
+           }
+           
+       }
+       
+   }
