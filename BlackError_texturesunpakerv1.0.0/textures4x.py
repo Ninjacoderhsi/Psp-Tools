f46d7aa44d4a2372b187b09c4aa354ea1bbdb6f4
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-29 16:36:29 +0430

    Add files via upload

---
+++ BlackError_texturesunpakerv1.0.0/textures4x.py
@@ -0,0 +1,18 @@
+import os
+import system
+import systemmake
+import javalib
+import object
+import object3D
+####SoftWarepsp Get File To Assete dont Edit File#####
+
+    if (get.rumse.path)+1=0
+    add namberfile dictatory()
+    }eles{
+        
+        nameber int1 = 0;
+        
+        pap max+01=12;
+        string file Dir rasu?0;
+        
+    }
