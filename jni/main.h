1499fb45179eae66f9173c4d2a4da959a20b38c6
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-29 13:56:54 +0430

    Add files via upload

---
+++ jni/main.h
@@ -0,0 +1,34 @@
+#include <main.cpp>
+#include "drawer"
+#include <main.h>
+#include <Files.java+cpp>
+
+using namespace srt;
+using namespace fot;
+
+int main()
+{
+    
+    _drawer.(background"#00000000#");
+    _drawer_linear1("context_app_game.java");
+    
+   _darwer_linear2("context_app_ppsspp.java");
+   _drawer_linear3("context_app_about.java");
+   _drawer_linear4("context_app_aboutmobile");
+    _drawer_linear5();
+    system.exit();
+    ///find
+}
+{
+    linear1(pastcolor("#000000"));
+    linear2(pastcolor("#000000"));
+    linear3(pastcolor("#0000000"));
+    linear4(pastcolor("#0000000"));
+    {
+        }
+            }
+               }         
+                  }
+                        }
+                          {
+
