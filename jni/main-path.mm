e3a45840eb6f8932152a60703c72d6b2b9f06872
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-31 10:38:03 +0430

    Add files via upload

---
+++ jni/main-path.mm
@@ -0,0 +1,28 @@
+#include <main.cpp>
+using namespace std;
+
+
+install using path data android{
+    
+    if delayMicroseconds map ();
+    findUntil digitalPinToInterrupt
+     bool = 301; 
+     }else{
+         
+         delayMicroseconds map();
+         folder getChannel ("t.me/psptools");
+        if ("/t".equals("0")) {
+	
+    }
+   else {
+	
+    }
+     }
+     if ("11".equals ("800"))
+     {
+         else{
+             
+         }
+     }
+     
+}
