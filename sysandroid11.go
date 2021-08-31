dc273a94c2363b83107140e471dd7338393087a6
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-08-31 12:07:57 +0430

    Add files via upload

---
+++ sysandroid11.go
@@ -0,0 +1,45 @@
+/////py File In Test Nof Runing Too App Ples/////
+
+import (
+ "system"
+ "makefile"
+ "part"
+ "steramfile"
+ "decode"
+
+)
+
+ app.coadenview("get.sdlmain.cpp")
+ fun 
+ try ( get.ry,path,$10000000000000,)
+ $120+0,,
+ Read files()
+ ReadLine.0~9999999999999
+ 
+ )
+ print(path+100#&222)
+ system os maderuan this.gep kv file;
+ _drawer_files.this.manager,storame file maker main.getlines,8000,
+ 
+ ( system make file get.string j ,
+ j , "psp",
+ 
+ )
+ decode = string decode file 
+ bool 0;
+ ( part make start app (bool 900;
+ 
+ )
+ (ani soft ware ruas path import j,fule.,
+ bool path = 11+3 = 0,
+ strame path get.master TAG file manager input in size manager get bool ,
+ bool 0,
+ 
+ if ( gaw.path decode file size manager(,0,0,0,0,0,0,0,0));
+ @null
+ (
+ nani.for.faind path main());
+ string scorollviwe1("color@null");
+ 
+ )
+ )
