2f87a668b5c592f65d737bdc04879bf95f27f9b3
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-03 01:20:26 +0430

    Add files via upload

---
+++ jni/apkicon.cpp
@@ -0,0 +1,19 @@
+#include <iconapp>
+int icon(){
+    
+    transmitterEnable 300;
+    try {
+        case 01;
+        App_Name = getType("pk_manager");
+        app_icon = getname("taskicon");
+        baud;
+        case 02;
+        file road("androidmainfast.xml");
+        baud; 
+    }
+    {
+        if ("File"("io"("port")));
+        analogReference bool 9000;
+        
+    }
+}
