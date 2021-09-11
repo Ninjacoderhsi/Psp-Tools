8c4cc3e371c85eff660fa03fc8f0784ca23c3c31
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-12 00:19:37 +0430

    Add files via upload

---
+++ Codec#/fun.go
@@ -0,0 +1,30 @@
+import(
+"os"
+"lyaout/main"
+"filedir"
+"shellroad"
+"borakdir"
+)
+int main()
+(
+ if new dir $0000000))))
+ main.xml bool path*
+ fun dir path)(((((((((()))))))))))
+ string fundango road"sh" = eoch?
+ get road (eoch,format,linocs,dir,primsino,sdcard))
+
+else
+fundango = 0,
+
+)
+     road,path(),
+              else
+     /sdcard/%s,
+get.dir(
+cat,sh,fan,gzip2,gunzip2,,,,
+)
+  )
+      )
+       )
+
+
