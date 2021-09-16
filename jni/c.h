63fab1eb01ba1dd51735d9ff9d5a00a7806e1b6d
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-17 00:12:22 +0430

    Add files via upload

---
+++ jni/c.h
@@ -0,0 +1,33 @@
+#include <dialog.cpp>
+using system compareTo Listview1()
+{
+   //////پیوند های لیست ویو 
+    listview1 = roadpath();
+    add item
+    png
+    jpg
+    iso
+    cso
+    mp4
+    mp3
+    tk
+    apng
+    gos
+    ;
+    
+}
+///// کلاس بازی و عکس ها
+     digitalPinToInterrupt Class
+     (@Void
+     )
+     all Typeimages{
+         png.apng.tk.gso
+         all TypeMedia{
+             mp3.mp4
+             {
+                 all TypeGames
+                 iso.cso
+             }
+         }
+     }
+     
---
+++ jni/dialog.cpp
@@ -0,0 +1,42 @@
+#include <c.h>
+IntervalTimer 500;
+
+
+
+
+ {
+     bool 800;
+     if 
+     not [string Dialog New ;
+     Dialog Add item ()
+     
+     Typeview Lyaout
+     Typeview Textview
+     Typeview Imageview
+     Typeview Textview1
+     Typeview Cardx
+     ]
+     #Grapic
+     Add item
+     
+      Cardx == new     
+      cardx.set.colorbackgrund(#fffffff)
+     Textview1 == textcolor("ff00000");
+     imageview == colse_setsize(64×64);
+     imageview == getType_setimge(abol.png);
+     
+     #onChange
+     
+     [ listview1.get_path()
+     
+     listview.goto filejava*Files.java
+     Add import #############
+     **************
+     in.java>>File>>java>>;
+     
+     ]
+     }else{
+         get.roadlistview1(null);
+     }
+     
+ }
