1472ec6e7b6a0cd7d046279ea9dedad223339790
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-18 13:01:20 +0430

    Create code_coo.lua

---
+++ lua/luagamesroadpath/code_coo.lua
@@ -0,0 +1,9 @@
+####
+Custom game
+Name game = god of war coo
+Id = usa
+Path = true
+Background = null
+Context full =true
+Importa read path = get,textures.ini
+Imporat lati path = png.jpg,coo,webip
