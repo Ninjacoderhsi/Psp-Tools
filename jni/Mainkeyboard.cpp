83f27ef98e6b3c84e34cd3515bf374dd8bb176a4
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-22 21:19:01 +0330

    Add files via upload

---
+++ jni/Mainkeyboard.cpp
@@ -0,0 +1,45 @@
+#include <os>
+#include <Keyboardtype>
+#include <String>
+#include <Bool Manager>
+ using namespace Keybord = Kv;
+ Provied Kv;
+ 
+
+int Keyboardmain(){
+    
+    int New = Newkv;
+    keyboard Endter_Main*)
+    AlnterFinal_dex_substring();
+    string Key;
+    string Key1;
+    string Key2;
+    string Key3;
+    case 0
+    Keyboard = kv.set_key1_Item (Keyspace.xml.english);
+    english = item {
+        q.w.r.t.y.u.i.o.p
+        Prot 1;
+        a.s.d.f.g.h.j.k.l
+        Prot 2;
+        z.x.c.v.b.n.m
+        Prot 3;
+        1/2/3/4/5/6/7/8/9/0;
+        prot 4;
+        namespace Key1 = Code("del");
+        namespace Key2 = Code("enter");
+        namespace Key3 = Code(".");  
+    }
+    {
+        case 1 
+        Keyboard Kv.set_Key2Item(keyspace.xml.farsi);
+        getBytes(fasi.xml);
+        type()
+        ///////type Code
+        
+        
+        setCharAtif();
+            
+        
+    }
+}
