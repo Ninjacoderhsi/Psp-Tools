d558437e055f0dfdeeffb4c2811d86772e48d446
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-09-26 13:39:06 +0330

    Update About.go

--- About.go
+++ About.go
@@ -6,59 +6,33 @@
 ///////are You Darveal?
 ///now Add Name For App Me/////
 
-import(
-"os"
-"backslash"
-"bolck nova"
-"androidunpker"
-"pather"
-"listmapmam"
+import (
+
+
+         "os"
+		 "makefile"
+		 "dier"
+		 "class.javapather"
+		 "synchronized"
+
 )
 
-
-
-
-if ( attachInterrupt.arry.list();
-
-get.niskname();
-   N== (ninjacoder);
-   n==(سازنده برنامه);
-   N==(black Error,Hirad,Hishame Diz,,,,
-   );
-   n==(پشتییانی انلاین.و خدمات. سرمایه گذار و خدمات.
-   run.to Dex9+java13= )
-)
-
- packet android Is(ture);
- 
- (Android listview On cklicket=(true));
- java compareTo(get.java.path(About.java));
- Sogetjava.is();
-  file.pathmaingettext+images.import(
-  
-  img1.img2.img3.img4);?
-  $hash
-  $findUntil
-  $Mainclass
-  $Mainfast
-  $getBytes
-  ()
-  (
-    listview.onChange(color.blue);
-    listview.onChange(blackerror);
-    (
-    get.text1==("من توسعه دهنده بخش گرافیک/‌n و خدمات انلاین برنامه هستم و در زمینه پایتون کار میکنم/n);
-    
-    )
-  listview1.get_speedmod("ture");
-  listview1.get.speed(203);
-  Liner2(240*fill);
-  $
-if[]
-fun Main Leokeyget(+00+1)
-int New i = 1+0=0;
-  /////////Code Find
-  
-  
-  
-  )
+        if ("extends" 0 + 0 )
+		main about default 0;
+		
+		add bot item 0
+		ninja coder ,
+		blackerror,
+		Hisham Dz,
+		Hirad,
+		Arab Ware,
+		Hosni Farj,
+		Abolefazl Killer,
+		Amir Travel;
+		
+		else
+		return if else item class
+		private item 00,
+		()()())));
+		stringbuling Hsi = new Hsi Send masg New add()
+		("fun")
