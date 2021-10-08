e81ef21b591b197334e62894f3f8491aefb4de95
lord redan <92160994+lord-readn@users.noreply.github.com>
2021-10-08 17:13:44 +0330

    Create foramemain.cpp

---
+++ jni/foramemain.cpp
@@ -0,0 +1,46 @@
+#include <iosteam>
+#include <s/game.c>
+#include <a/assert.h>
+#include <s/string.h>
+{
+	class new  layout manager ()
+	
+	final new layout manager = new()
+	private files;
+	package org.ppsspp.ppsspp,
+	private xml;
+	private yml;
+	private package name app = @null
+	}
+	
+	if  ("g")+ (0) ;
+	new arry listmap <<listmap1>> 
+	new object map << liststring>>
+	if  true > 0 + > 0 > 0 / 1;
+	is game fast gaming(),
+	stringbullider new stringbullider = work()
+	const select app (+org.ppsspp.ppsspp(0);
+	}
+	{
+		{
+			},
+			{
+				},
+				return 0;
+				import class = layout manager();
+				{
+					},
+					class new Audio manager ()
+					Audio manager add item new super 0;
+					if else {
+						Audio manager user math File () + 0,
+						return false,
+						{
+							Audio manager (listmap1) <<$>>
+							assert.get.goto.files(Compliter.ini);
+							 Audio manager onCq();
+							
+							
+						
+					}
+					 
