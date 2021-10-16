75dd48834f8008f95b3d861fa7ade760caf01dcb
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-10-16 14:25:08 +0330

    update AndroidEGLContext.h

--- ninjacoderlib/AndroidEGLContext.h
+++ ninjacoderlib/AndroidEGLContext.h
@@ -3,7 +3,16 @@
 #include "Common/GPU/OpenGL/GLRenderManager.h"
 #include "AndroidGraphicsContext.h"
 #include "Common/GL/GLInterfaceBase.h"
+#include "android/private/di/g.h"
 
+class ninjacoder :
+private ninjacoder 
+
+file fake() primsinone file://sdcard/psp;
+  java.Common.ppssppAtivicty();
+  if (!Data,null)
+  string srt1 = "sorry dont primsinone/n app me ,"
+  std::cout << "srt1" << std::endl;
 class AndroidEGLGraphicsContext : public AndroidGraphicsContext {
 public:
 	AndroidEGLGraphicsContext() : draw_(nullptr), wnd_(nullptr), gl(nullptr) {}
@@ -42,4 +51,9 @@ private:
 	ANativeWindow *wnd_;
 	cInterfaceBase *gl;
 	GLRenderManager *renderManager_ = nullptr;
-};
+}
+ private : 
+ Draw :: GetDrawContext *symi;
+ AlnterDialog*DIALOG_typedef.androif11
+ nullptr;
+ };
