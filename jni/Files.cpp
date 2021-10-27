502cd621bc6cfa797ce32864712f74f44373f9f9
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-10-27 19:31:36 +0330

    update Files.cpp

--- jni/Files.cpp
+++ jni/Files.cpp
@@ -1,1 +1,55 @@
-///
+#include <temp.h>
+#include <stdlib.h>
+#include <list>
+#define "iostream"
+#define "main.cpp"
+#define "c.h"
+
+int Files();
+
+private listview1,
+private lin1,
+private img1,
+private img2,
+  public appcompant androidx()
+  and ++ androidbar(true)
+  {
+    if (int (Get.zp(0)))
+    friend
+    end;
+    else if(TRUE)
+    GET.Imaginary.Files.java()
+    {
+      string h == h1,
+      h1 == import base 64 mod ,
+      stusebar.get.color(Color(color.paser(#bb0000)));
+      this.appGet.listbar(
+        [
+          (.png),
+          (.jpg),
+          (.ini),
+          (.mp4),
+          (.apk),
+          (.apkx),
+          
+          
+          ];
+          
+          listOnClickItem(){
+            [
+              itemadd.import.get.Files.java()
+              this.getline(9~100);
+              
+      /// add javafile 
+      
+      
+      
+              intent i = i();
+              i.setclass(get.app(this.exsrt));
+              ]
+          }
+        
+        )
+      
+    }
+  }
