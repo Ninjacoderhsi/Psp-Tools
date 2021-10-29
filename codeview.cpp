e98fb763b47eef1dffa7cbec6cc238f0071f479c
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-10-29 11:46:49 +0330

    update codeview.cpp

--- codeview.cpp
+++ codeview.cpp
@@ -1,102 +1,27 @@
 #include <iostream>
-#include <C.h>
-using namespace std;
+#include <stdho.h>
+#include "codeview.h"
 
-int Code_viewr{
-    pv"get.path();
-    pv"get.man();
-    pn"get.substring();
-}
+#define "ios.h"
+#define "h.mm"
+
+int m ()
+
+if(eumlutor.codeview && 00 |run.time)
 {
-    
-    
-    img1 == new Final getBytes(1024*1024)
-    img1 == Main.cpp(get.ppsspp.java);
-    img2 == Main.cpp.(get.ppsspp.java);
-    img3 == main.cpp.(get.ppsspp.java);
-    
-    if (Get.cpu.ram(1)2(3(4(5(6(7(8(9(10(11(12?13(14(15(16(18)))))))))))))));
-    
-    android.mvk_getRam();
-    }eles{
-        
-        if 2++i();
-        startsWith2021*2020;
-        onChange.codeview();
-        @void
-        (Get.import(system.tools);
-        
-        C.get.new_network();
-        network(https//:@tasknull);
-        {
-            androidgetcontext(codeviwer_pspfotmat[LUA]);
-            constrain(text_Psp_Hash)+1()=path.textures.ini;
-            
-            string Textures = Textures();
-            
-            
-           //// *******
-           
-           texturesini == hash<Map>
-           new Map <texturesini>
-           map Get.path[];
-           
-           [ [hash]
-           000000000000?();
-           png.jpg.webp.bmp();
-           path_file_other_format(/apptype);
-           
-           
-           ]
-           {
-               context(satave.codeview);
-               mainis Path[1+1÷0=11];
-               bool=201;
-               if (0);
-               context(satave.codeview);
-               mainis Path[1+0+0=13];
-               bool=0;
-               endl;
-               cout << error << endl;
-               if (33);
-               
-               case 001;
-               path11 = getBytes(c);
-               
-               case 002;
-               path =getType(0~9999999999999);
-               
-               case 003;
-               edittext1 = getBytes(edittextnameber.java);
-               case 004;
-               Final Edittext soheil = soheil ();
-               Get.typeview(getliner1~appt1+"~");
-               case 005;
-               onChange();
-               onResume();
-               onstop();
-               onpause();
-               {
-                   get.onResume(ge_context_pathfile);
-                   return (true);
-                  if ("".equals("")) {
-	
+  run.time$ { install codeview $ 0 )
+  thread_local == s ,
+  int mai = 12 ;
+  bool = 9120;
+   string hhh= hhh;
+   hhh == ms//: bits/stdc++.hcard(?)
 }
-else {
-	return (FileUtil.isFile("Path(c\"cd/psp\");"));
+else
+{
+  try{
+    install fbps main mjj path = fbps main mjj path € get.byte();
+    }catch(cansert e ){
+      std::cout << e.error << std::endl;
+  }
 }
-
- DynamicLinks.getInstance().
- getDynamicLink(codeview.this.getIntent
-   ()).addOnSuccessListener(codeview.this, dc_onSuccessLink).addOnFailureListener(codeview.this, dc_onFailureLink);
-
-               }
-               
-           }
-            
-            
-        }
-        
-        
-    }
-}
+endl;
