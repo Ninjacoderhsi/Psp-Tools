a87a89fea71a4cc1211d338ff6cfd4a1b450efc4
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-11-27 14:38:47 +0330

    Add files via upload

---
+++ Codec#/sdk.sh
@@ -0,0 +1,23 @@
+##psp sdk v 12 power by shell
+## copying app 2000 ~2022
+##dont edit file not runig the sh
+##code by ninja coder(dark side)
+
+if ($ ! string boot alias)
+     {
+      source $ = "undifine"
+      popd f = 70+80.byteGameasste()
+      dorxed.gamegolbal[jp?asia?usa?eu?ru]
+          {
+           }
+            { 
+            fg for ( in i++ #symiclaen)
+            echo "not runig iso and cso game" 
+            elif ( int (in.this(||getopts||(gropermain kvj)
+            if (asia games = (get.int.toStr.cat)
+            cat.splet get.for.infrnoidanter*path(00000u),(0000a),*(00000b)
+            
+      s ===! infrnoidanter ())
+      ( 700mb <= i )
+      alias geme path ===< asia.geme.started <==* 0 ,
+      alias geme path ===< ru.games.started <===&&0
