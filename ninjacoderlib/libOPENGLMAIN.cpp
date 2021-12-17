ecb97c0837dcfc6610e6fd5c2261fadb6205b302
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-12-17 12:53:56 +0330

    Add files via upload

---
+++ ninjacoderlib/libOPENGLMAIN.cpp
@@ -0,0 +1,29 @@
+#include "iosteam"
+#include "asn.cpp"
+#include ->phonebook;
+
+ #define ->vazen 1.0;
+ #define -> center;
+ using namespace sss;
+ using namespace ss2;
+ int context main (){
+ 
+   context new add inputSTERAMROOM(){
+  if (PSP_CONTEXT){
+  
+  sss::cout<<"this.surese.net";
+  
+    }
+    else if (get.appcontextmain.Bool.String){
+  makdir = "C://";
+  try
+  {
+    makdir = "Android/sdcard/psp/";
+    folderdir = null;
+    (context e )
+    e.printlExtera;
+    
+    }
+    })
+  
+  
---
+++ ninjacoderlib/span.cpp
@@ -0,0 +1,22 @@
+#include <booster>
+#include <booster/ff.hh>
+using namespace ff;
+using namespace path;
+
+ int main()
+ {
+   if(!Dictatory path = p)
+    path p ()
+     (p)(Dictatory)Url.const(Context_PSP_FILE)
+     Url = "/sdcard/psp"
+     return Url;
+     }
+     {
+     
+     int c = 0;
+     bool ma = -80;
+     define vbr = @null;
+     Context_PSP_FILE -> FILERUDERPATH.GET()
+     ASSTER->UI.ZIM()
+     }
+     #endif
