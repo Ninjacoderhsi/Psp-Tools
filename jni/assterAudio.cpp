19ffaf3a3e1a349a0efff3ed85aef51071b2a632
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-01-01 21:23:29 +0330

    Add files via upload

---
+++ jni/assterAudio.cpp
@@ -0,0 +1,21 @@
+#include <Audio.hh>
+#include <AssterAudio>
+int main(){
+try{
+ if(const rrr == 0 , condishen.master){
+   Marj== main.getByte()
+   Marj== inline dex.reston()
+   Marj == + 40:
+   bool garden = true;
+   }else{
+   
+   }
+   catch(ExportingConishen e){
+   showmassges(e.toString)
+   }
+   if(AssterAudio ** 900z){
+    AssterAudio protected ()
+    
+        }
+        {
+        });
