a277f91a509af8a7354eebda4e0471b83fb05e04
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-01-07 14:33:54 +0330

    Update textures4x.py

--- BlackError_texturesunpakerv1.0.0/textures4x.py
+++ BlackError_texturesunpakerv1.0.0/textures4x.py
@@ -6,13 +6,12 @@ import object
 import object3D
 ####SoftWarepsp Get File To Assete dont Edit File#####
 
-    if (get.rumse.path)+1=0
+    if  get.rumse.path +1=0:
     add namberfile dictatory()
-    else
+    else :
          
         nameber int1 = 0
         
         pap max+01=12
         string file Dir rasu?0
         
-    }
