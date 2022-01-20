cbf6d5133def150ca378a010e1de6577ab5a6be9
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-01-20 21:56:40 +0330

    Add files via upload

---
+++ jni/R1.cpp
@@ -0,0 +1,22 @@
+#include "core.h"
+#include <iosteam>
+ using namespace R;
+ {
+ if(Audio != get.asste(gameContorol.ini)(int a = 40)){
+      GETPOSTAUDIO.GETFORMATTHIS.null(char16_t = ! return 0);
+      char32_t MASTERAUDIO.THIS.ANTIFI.GET.BYTE.THIS(@null)
+      
+   
+   
+   }else{
+     for ( a < 80 >= volom a++){
+        public class Funsheild(){
+        
+        
+        }
+        
+     
+     }
+    
+     
+   }
