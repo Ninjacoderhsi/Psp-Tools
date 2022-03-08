25645fbdce57315e6f9a56c7cd2f28955e64c3c1
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2022-03-08 20:14:22 +0330

    Update texturescomplier.py

--- texturescomplier.py
+++ texturescomplier.py
@@ -6,13 +6,5 @@ import object
 import object3D
 ####SoftWarepsp Get File To Assete dont Edit File#####
 
-    if (get.rumse.path)+1=0
-    add namberfile dictatory()
-    }eles{
-        
-        nameber int1 = 0;
-        
-        pap max+01=12;
-        string file Dir rasu?0;
-        
-    }
+    
+    
